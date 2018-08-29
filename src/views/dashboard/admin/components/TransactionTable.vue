3c52959c1ec44a5f7e9071a9f167a8a4d0c0f6f5
mohevha <511923360@qq.com>
2018-08-29 23:30:31 +0800

    1

---
+++ src/views/dashboard/admin/components/TransactionTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-table :data="list" style="width: 100%;padding-top: 15px;">
+    <el-table-column label="订单序号" min-width="200">
+      <template slot-scope="scope">
+        {{ scope.row.order_no | orderNoFilter }}
+      </template>
+    </el-table-column>
+    <el-table-column label="成交价格" width="195" align="center">
+      <template slot-scope="scope">
+        ¥{{ scope.row.price | toThousandslsFilter }}
+      </template>
+    </el-table-column>
+    <el-table-column label="状态" width="100" align="center">
+      <template slot-scope="scope">
+        <el-tag :type="scope.row.status | statusFilter"> {{ scope.row.status }}</el-tag>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import { fetchList } from '@/api/transaction'
+
+export default {
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        success: 'success',
+        pending: 'danger'
+      }
+      return statusMap[status]
+    },
+    orderNoFilter(str) {
+      return str.substring(0, 30)
+    }
+  },
+  data() {
+    return {
+      list: null
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      fetchList().then(response => {
+        this.list = response.data.items.slice(0, 8)
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/layout/components/FooterBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="main">
+Copyright © 2018 上海开创广告有限公司 版权所有 All rights reserved. <svg-icon icon-class="mit" class="card-panel-icon"  />
+<span class="build"><svg-icon icon-class="building" class="card-panel-icon" /></span>
+<span class="npm"><svg-icon icon-class="npm" class="card-panel-icon" /></span>
+</div>
+</template>
+<script>
+export default {
+    name: 'FooterBar'
+}
+</script>
+<style scoped>
+.main{
+ text-align: center; padding-top: 10px; padding-bottom: 10px;
+ font-size: 14px;
+ background: #f0f2f5;
+ color: #909399;
+}
+.main >>> .svg-icon{
+   width: 3.9em; height:1em;
+}
+.build >>> .svg-icon{
+   width: 5.3em; height:1em;
+}
+.npm >>> .svg-icon{
+   width: 4.4em; height:1em;
+}
+
+</style>
